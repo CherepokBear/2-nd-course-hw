4ed757be92b425513bccb8b612fbd33b9ff3e466
CherepokBear <sibgufk684@gmail.com>
2023-04-09 18:35:38 +0300

    Commit JS

---
+++ hw-2.js
@@ -0,0 +1,50 @@
+let a = 10;
+alert(a);
+
+let iphone = 2007;
+alert(iphon);
+
+let one = 10;
+let two = 20;
+alert((one - two));
+alert((one + two));
+alert((one * two));
+alert((one / two));
+
+let degree = 2;
+let result = pow(degree, 5);
+alert(result);
+
+let a = 9;
+let b = 2;
+let result = a % b;
+alert(result);
+
+let num = 1;
+num = num + 5;
+num = num - 3;
+num = num * 7;
+num = num / 3;
+num = num + 1;
+num = num - 1;
+alert(num);
+
+let age = prompt("Сколько вам лет?");
+alert(age);
+
+let user = {
+    name: Хризантем,
+    age: 284,
+    isAdmin: true
+  };
+
+  user.city_of_residence;
+  user[age] = 35;
+
+  delete user.city_of_residence;
+
+  let info = prompt("Какую информацию хотите узнать o пользователе?");
+  alert(info);
+
+  let c = prompt("Ваше имя");
+  alert("Привет" + a + "!");
