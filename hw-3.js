5bf8bbb56300c2279a497d2bedc253d6b44b61d5
CherepokBear <sibgufk684@gmail.com>
2023-04-12 17:29:32 +0300

    homwork-3

---
+++ hw-3.js
@@ -0,0 +1,22 @@
+let password = 'пароль';
+let pws = prompt("Введите пароль");
+if (condition) {
+    console.log("Пароль введен верно");
+} else {
+    console.log("Пароль введен неправильно");
+}
+
+let c = 35;
+if (c > 0 && c < 10) {
+    console.log("верно");
+} else {
+    console.log("неверно");
+}
+
+let d = 35;
+let e = 25;
+if (d || e > 100) {
+    console.log("верно");
+} else {
+    console.log("неверно");
+}
