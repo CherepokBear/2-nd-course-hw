72765a3704ca415441a67c3f7a629ea70054bb0a
CherepokBear <sibgufk684@gmail.com>
2023-04-13 14:30:16 +0300

    Доделаный JS

--- hw-3.js
+++ hw-3.js
@@ -20,3 +20,52 @@ if (d || e > 100) {
 } else {
     console.log("неверно");
 }
+
+let a = 2;
+let b = 3;
+let num = Number(a + b);
+alert(num);
+
+let jan = "Январь";
+let feb = "Февраль";
+let mar = "Март";
+let apr = "Апрель";
+let may = "Май";
+let jun = "Июнь";
+let jul = "Июль";
+let aug = "Август";
+let sep = "Сентябрь";
+let oct = "Октябрь";
+let nov = "Ноябрь";
+let dec = "Декабрь";
+
+let month = prompt("Ведите первые три буквы месяца по английски");
+
+if (month === "jan") {
+    console.log("Январь")
+}else if  (month === "feb"){
+    console.log("Февраль")
+}else if (month === "mar") {
+    console.log("Март")
+}else if (month === "apr") {
+    console.log("Апрель")
+}else if (month === "may") {
+    console.log("Май")
+}else if  (month === "jun"){
+    console.log("Июнь")
+}else if (month === "jul") {
+    console.log("Июль")
+}else if  (month === "aug"){
+    console.log("Август")
+}else if (month === "sep") {
+    console.log("Сентябрь")
+}else if (month === "oct") {
+    console.log("Октябрь")
+}else if (month === "nov") {
+    console.log("Ноябрь")
+}else if{
+    console.log("Декабрь")
+}else {
+    console.log("No release")
+}
+
